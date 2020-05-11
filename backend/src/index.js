4600a0e228f18dffe82b55444ef91eb2a69d0ed7
Nino Keldishvili <ninokeldishvili@gmail.com>
2020-05-12 00:25:24 +0400

    Add Navbar, Questions and Question components. Made routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -71,7 +71,6 @@ app.post('/answer/:id', (req, res) => {
 });
 
 // start the server
-
 app.listen(8081, () => {
   console.log('listening on port 8081');
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+import NavBar from './NavBar/NavBar';
+import Question from './Question/Question';
+import Questions from './Questions/Questions';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Work in progress.</p>
+        <NavBar/>
+        <Route exact path='/' component={Questions}/>
+        <Route exact path='/question/:questionId' component={Question}/>
       </div>
     );
   }
